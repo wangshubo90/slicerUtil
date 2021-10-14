28ecb57e5d7a86bd69916cd7df9dad883936f944
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2021-10-13 23:30:39 -0500

    update module

--- module.py
+++ module.py
@@ -2,7 +2,7 @@ import re
 import vtk, qt, ctk, slicer
 from SegmentMesher import SegmentMesherLogic
 from slicer.ScriptedLoadableModule import *
-from naiveSegment import naiveSegment
+import SimpleITK as sitk
 
 def findProperty(obj, re_patter):
     return [i for i in dir(obj) if re.search(re_patter, i)]
@@ -73,6 +73,67 @@ def capture3Dview(outputfile):
     writer.SetFileName(outputfile)
     writer.SetInputConnection(wti.GetOutputPort())
     writer.Write()
+
+def naiveSegment(masterVolumeNode,
+                segmentEditorNode=None, 
+                segmentEditorWidget=None, 
+                segName="", 
+                segMapName="bone", 
+                to_file=None, 
+                lowerThreshold=30, 
+                upperThreshold=255, 
+                smoothSigma=6, 
+                color=(0.5,0.5,0.5),
+                keep_largest_island=False, 
+                cleanUp=False):
+    # Create segmentation
+    segmentationNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode")
+    # segmentationNode.CreateDefaultDisplayNodes() # only needed for display
+    segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(masterVolumeNode)
+    segmentationNode.SetName(segName)
+    addedSegmentID = segmentationNode.GetSegmentation().AddEmptySegment(segMapName)
+    segmentationNode.GetSegmentation().GetSegment(addedSegmentID).SetColor(*color)
+
+    # Create segment editor to get access to effects
+    if segmentEditorWidget == None:
+        segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+    segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+    if segmentationNode == None:
+        segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
+    segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
+    segmentEditorWidget.setSegmentationNode(segmentationNode)
+    segmentEditorWidget.setMasterVolumeNode(masterVolumeNode)
+
+    # Thresholding
+    segmentEditorWidget.setActiveEffectByName("Threshold")
+    effect = segmentEditorWidget.activeEffect()
+    effect.setParameter("MinimumThreshold", lowerThreshold)
+    effect.setParameter("MaximumThreshold", upperThreshold)
+    effect.self().onApply()
+
+    # Smoothing
+    segmentEditorWidget.setActiveEffectByName("Smoothing")
+    effect = segmentEditorWidget.activeEffect()
+    effect.setParameter("SmoothingMethod", "Gaussian")
+    effect.setParameter("KernelSizeMm", smoothSigma)
+    effect.self().onApply()
+
+    if keep_largest_island:
+        segmentEditorWidget.setActiveEffectByName("Islands")
+        effect.setParameter("Operation", "KEEP_LARGEST_ISLANDS") # check other parameters https://slicer.readthedocs.io/en/latest/developer_guide/modules/segmenteditor.html
+        effect.self().onApply()
+
+    if cleanUp:
+        segmentEditorWidget = None
+        slicer.mrmlScene.RemoveNode(segmentEditorNode)
+
+    # Make segmentation results visible in 3D
+    segmentationNode.CreateClosedSurfaceRepresentation()
+
+    if to_file:
+        slicer.util.saveNode(segmentationNode, to_file)
+    else:
+        return segmentationNode, segmentEditorNode, segmentEditorWidget
 
 class SegmentMesher3D(ScriptedLoadableModuleTest):
     """
